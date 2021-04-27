91a971b0594de95bf7eeeb80e682ae3cb7844b49
Syamsul <sam.project99@gmail.com>
2021-04-27 15:45:07 +0700

    Update header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -47,13 +47,13 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="#">LalajoFilm</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Link</a>
+                <li class="active"><a href="#">Home</a>
                 </li>
                 <li><a href="#">Link</a>
                 </li>
