cb3f92ee34997eac34074c1263f919c1d8ae52ff
Syamsul <sam.project99@gmail.com>
2021-05-04 14:41:56 +0700

    add kodebooking

--- application/models/M_invoice.php
+++ application/models/M_invoice.php
@@ -2,10 +2,11 @@
 class M_invoice extends CI_model
 {
 
-  // Mengembalikan seluruh kode film berdasarkan ID film. 
+  // Mengembalikan seluruh kode film berdasarkan ID film.
   public function getKodeById($id)
   {
     $this->db->where('IdInvoice', $id);
     return $this->db->get('Kode')->row_array();
   }
+
 }
