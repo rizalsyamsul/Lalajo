db27807f58d1cde826c39e55f5afc58a0b9cb370
raihanns <rainsdotnet@gmail.com>
2021-05-04 15:13:01 +0700

    Update Transaksi.php

--- application/controllers/Transaksi.php
+++ application/controllers/Transaksi.php
@@ -13,10 +13,33 @@ class Transaksi extends CI_Controller
     }
 
     /**
-     * Memuat view halaman transaksi
+     * Memuat view halaman transaksi pembayaran
      */
-    public function display()
+    public function index()
     {
-        $this->load->view('Transaksi');
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+        $data['title'] = 'Pembayaran';
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('Pembayaran', $data);
+        $this->load->view('templates/footer');
     }
+
+    public function pembayaran()
+    {
+
+        if ($transaksi['status'] == 'success') {
+            $data = [
+                'film' => $this->input->post('deskripsi', true),
+                'jumlah' => $this->input->post('rating', true),
+                'harga' => $this->input->post('judul'),
+                'total' => $this->input->post('kategori', true)
+            ];
+            $this->M_Transaksi->insertTransaksi($data);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Selamat! Pembayaran berhasil</div>');
+            redirect('Pembayaran/success');
+        }else{
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Pembayaran gagal!</div>');
+            redirect('Pembayaran/cancel');
+        }
 }
