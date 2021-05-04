2f744bc5d108a81eecc677008d850a0c1c4dfac0
raihanns <rainsdotnet@gmail.com>
2021-05-04 15:03:34 +0700

    Update M_transaksi.php

--- application/models/M_transaksi.php
+++ application/models/M_transaksi.php
@@ -4,46 +4,29 @@ class M_transaksi extends CI_model
 {
 
   // Memasukkan data film ke dalam database
-  public function insertFilm($data)
+  public function insertTransaksi($data)
   {
-    return $this->db->insert('film', $data);
+    return $this->db->insert('transaksi', $data);
   }
 
   // Mengupdate data film berdasarkan id film
-  public function editFilm($id)
+  public function editTransaksi($id)
   {
     // Data akan di input secara post ke dalam database
     $data = [
-      'deskripsi' => $this->input->post('deskripsi', true),
-      'rating' => $this->input->post('rating', true),
-      'judul' => $this->input->post('judul'),
-      'kategori' => $this->input->post('kategori', true)
+      'film' => $this->input->post('deskripsi', true),
+      'jumlah' => $this->input->post('rating', true),
+      'harga' => $this->input->post('judul'),
+      'total' => $this->input->post('kategori', true)
     ];
-    $this->db->where('idFilm', $id);
-    $this->db->update('film', $data);
-  }
-
-  // Memasukkan data kursi ke dalam database
-  public function insertKursi($data)
-  {
-    return $this->db->insert('kursi', $data);
-  }
-
-  // mengupdate data kursi dalam database berdasarkan ID Kursi
-  public function editKursi($id)
-  {
-    // Data akan di input secara post ke dalam database
-    $data = [
-      'noKursi' => $this->input->post('noKursi', true)
-    ];
-    $this->db->where('idKursi', $id);
-    $this->db->update('kursi', $data);
+    $this->db->where('idTransaksi', $id);
+    $this->db->update('transaksi', $data);
   }
 
   // Menghapus data kursi di database berdasarkan ID Kursi
-  public function deleteKursi($id)
+  public function deleteTransaksi($id)
   {
-    $this->db->where('idKursi', $id);
-    $this->db->delete('kursi');
+    $this->db->where('idTransaksi', $id);
+    $this->db->delete('transaksi');
   }
 }
