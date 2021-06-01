c82cb18e928185389054d181b70120ec5849c737
Ridhwankiki <ridhwanr0607@gmail.com>
2021-06-01 23:02:31 +0700

    Update Pembayaran,php

--- application/views/book/payment.php
+++ application/views/book/payment.php
@@ -3,7 +3,41 @@
 
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+    <div class="card mb-3" style="max-width: 540px;">
+        <div class="row no-gutters">
+            <div class="col-md-4">
+                <img src="<?= base_url('/assets/img/movies/MortalKombat.jpg') ?>" class="card-img" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">Mortal Kombat</h5>
+                    <p class="card-text">XXI Transmart Buah Batu</p>
+                    <p class="card-text"><small class="text-muted">Senin, 31 May 2021</small></p>
+                    <p class="card-text"><small class="text-muted">D1, D2</small></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <div class="card mb-3" style="max-width: 540px;">
+        <div class="row no-gutters">
+            <div class="col-md-4">
+                <img src="<?= base_url('/assets/img/movies/MortalKombat.jpg') ?>" class="card-img" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">Mortal Kombat</h5>
+                    <p class="card-text">XXI Transmart Buah Batu</p>
+                    <p class="card-text"><small class="text-muted">Senin, 31 May 2021</small></p>
+                    <p class="card-text"><small class="text-muted">D1, D2</small></p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 <!-- /.container-fluid -->
 
