657d19f377311b4f2e8a61129b20651a5fcacc5d
raihanns <rainsdotnet@gmail.com>
2021-06-01 23:16:47 +0700

    Update Main.php

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -21,6 +21,7 @@ class Main extends CI_Controller
 
 	public function theater()
 	{
+		$data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
 		$data['title'] = 'Theater of LalajoFilm';
 		$this->load->view('templates/main_header', $data);
 		$this->load->view('Theater');
