1c9050a41a579ec81cfe4b89d70f63a5eb9f52ea
raihanns <rainsdotnet@gmail.com>
2021-06-15 13:11:48 +0700

    report

--- application/views/admin/report.php
+++ application/views/admin/report.php
@@ -6,72 +6,40 @@
 
     <div class="card shadow mb-4">
         <div class="card-header py-3">
-            <h6 class="m-0 font-weight-bold text-primary">Monthly Report</h6>
+            <h6 class="m-0 font-weight-bold text-primary">Report</h6>
         </div>
         <div class="card-body">
             <div class="table-responsive">
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                     <thead>
                         <tr>
+                            <th>No</th>
                             <th>Name</th>
-                            <th>Position</th>
-                            <th>Office</th>
-                            <th>Age</th>
-                            <th>Start date</th>
-                            <th>Salary</th>
+                            <th>Film</th>
+                            <th>Cinema</th>
+                            <th>Date</th>
+                            <th>Theater</th>
+                            <th>Seats</th>
+                            <th>Price</th>
+                            <th>Code</th>
                         </tr>
                     </thead>
-                    <!-- <tfoot>
-                        <tr>
-                            <th>Name</th>
-                            <th>Position</th>
-                            <th>Office</th>
-                            <th>Age</th>
-                            <th>Start date</th>
-                            <th>Salary</th>
-                        </tr>
-                    </tfoot> -->
                     <tbody>
-                        <tr>
-                            <td>Tiger Nixon</td>
-                            <td>System Architect</td>
-                            <td>Edinburgh</td>
-                            <td>61</td>
-                            <td>2011/04/25</td>
-                            <td>$320,800</td>
-                        </tr>
-                        <tr>
-                            <td>Garrett Winters</td>
-                            <td>Accountant</td>
-                            <td>Tokyo</td>
-                            <td>63</td>
-                            <td>2011/07/25</td>
-                            <td>$170,750</td>
-                        </tr>
-                        <tr>
-                            <td>Ashton Cox</td>
-                            <td>Junior Technical Author</td>
-                            <td>San Francisco</td>
-                            <td>66</td>
-                            <td>2009/01/12</td>
-                            <td>$86,000</td>
-                        </tr>
-                        <tr>
-                            <td>Cedric Kelly</td>
-                            <td>Senior Javascript Developer</td>
-                            <td>Edinburgh</td>
-                            <td>22</td>
-                            <td>2012/03/29</td>
-                            <td>$433,060</td>
-                        </tr>
-                        <tr>
-                            <td>Airi Satou</td>
-                            <td>Accountant</td>
-                            <td>Tokyo</td>
-                            <td>33</td>
-                            <td>2008/11/28</td>
-                            <td>$162,700</td>
-                        </tr>
+                        <?php $i = 1; ?>
+                        <?php foreach ($report as $r) : ?>
+                            <tr>
+                                <td><?= $i; ?></td>
+                                <td><?= $r['name'] ?></td>
+                                <td><?= $r['film'] ?></td>
+                                <td><?= $r['bioskop'] ?></td>
+                                <td><?= $r['date'] ?></td>
+                                <td><?= $r['theater'] ?></td>
+                                <td><?= $r['kursi'] ?></td>
+                                <td><?= $r['harga'] ?></td>
+                                <td><?= $r['code'] ?></td>
+                            </tr>
+                            <?php $i++; ?>
+                        <?php endforeach; ?>
 
                     </tbody>
                 </table>
